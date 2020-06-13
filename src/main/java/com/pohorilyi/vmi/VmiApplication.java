6e85cee04cb7e3e243567a8a340d762c42b341c9
Yurii Pohorilyi <pogoriliyyuriy@gmail.com>
2020-06-13 18:21:39 +0300

    Init commit

---
+++ src/main/java/com/pohorilyi/vmi/VmiApplication.java
@@ -0,0 +1,13 @@
+package com.pohorilyi.vmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmiApplication.class, args);
+	}
+
+}
